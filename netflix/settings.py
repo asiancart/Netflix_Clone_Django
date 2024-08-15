3b56add2408a8dc431449ee16e1964f228fa45ae
asiancart <96198489+asiancart@users.noreply.github.com>
2024-08-15 21:14:05 +0300

    Update settings.py

--- netflix/settings.py
+++ netflix/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-jzxv@^^a965!=bryh405l5tv&a36_ej0f@s%8iy0j79#+x39qf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
